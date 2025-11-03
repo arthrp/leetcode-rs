c3ad2d430adba2baf01feac4d8d75e49c1b2307c
Arthur <arthrp@users.noreply.github.com>
2025-11-04 01:03:41 +0200

    Reverse integer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![allow(dead_code)]
 mod two_sum_solution;
 mod palindromic_substring_solution;
-mod zigzag;
+mod zigzag_solution;
+mod reverse_integer_solution;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/reverse_integer_solution.rs
@@ -0,0 +1,33 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn reverse(mut x: i32) -> i32 {
+        let mut rev: i32 = 0;
+
+        while x != 0 {
+            let pop = x % 10;
+            x /= 10;
+
+            if rev > i32::MAX / 10 || (rev == i32::MAX / 10 && pop > 7) {
+                return 0;
+            }
+            if rev < i32::MIN / 10 || (rev == i32::MIN / 10 && pop < -8) {
+                return 0;
+            }
+
+            rev = rev * 10 + pop;
+        }
+        rev
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_reverse() {
+        assert_eq!(Solution::reverse(123), 321);
+        assert_eq!(Solution::reverse(-123), -321);
+    }
+}
